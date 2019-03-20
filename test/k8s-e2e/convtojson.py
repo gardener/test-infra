5390f4a84bf0df77b81e447669a3579651303515
Oleg Loewen <oleg.loewen@sap.com>
2019-03-20 09:46:43 +0100

    Introduce slow and fast e2e base descriptions
    add testgrid_publish test definition

--- test/k8s-e2e/convtojson.py
+++ test/k8s-e2e/convtojson.py
@@ -19,50 +19,54 @@ import os
 import getopt
 import uuid
 import re
+import glob
 
 
 def convert_junit_01_xml_to_json(
-        inputfile, index, targetdir, descfile):
+        inputdir, index, targetdir, descfile):
 
     try:
-        with open(inputfile) as inputfile_reader:
-            doc = xmltodict.parse(inputfile_reader.read(), attr_prefix='', cdata_key='text',
-                                  dict_constructor=dict)
+        junit_files = glob.glob(inputdir + "/junit*.xml")
+        for junit_file in junit_files:
+            if "junit_runner.xml" in junit_file:
+                continue  # special case
+            with open(junit_file) as junit_file_reader:
+                # force_list is required, otherwise xmltodict would return a single element instead of a list in case there is only one testcase in xml
+                doc = xmltodict.parse(junit_file_reader.read(), attr_prefix='', cdata_key='text', dict_constructor=dict, force_list={'testcase': True})
+            for testcase in doc['testsuite']['testcase']:
+                if 'skipped' not in testcase:
 
-        for testcase in doc['testsuite']['testcase']:
-            if 'skipped' not in testcase:
-
-                # Generate unique filename
-                filename = os.path.join(targetdir, 'test-{}.json'.format(str(uuid.uuid4())[0:6]))
-                c = 0
-                while os.path.isfile(filename):
+                    # Generate unique filename
                     filename = os.path.join(targetdir, 'test-{}.json'.format(str(uuid.uuid4())[0:6]))
-                    if c > 50:
-                        help('ERROR: random filename for test result could not be generated')
-                        sys.exit(1)
-                    c += 1
+                    c = 0
+                    while os.path.isfile(filename):
+                        filename = os.path.join(targetdir, 'test-{}.json'.format(str(uuid.uuid4())[0:6]))
+                        if c > 50:
+                            help('ERROR: random filename for test result could not be generated')
+                            sys.exit(1)
+                        c += 1
 
-                # Create target json structure
-                res = {}
-                if 'failure' in testcase:
-                    res['status'] = 'failure'
-                else:
-                    res['status'] = 'success'
+                    # Create target json structure
+                    res = {}
+                    if 'failure' in testcase:
+                        res['status'] = 'failure'
+                    else:
+                        res['status'] = 'success'
 
-                for key, value in testcase.items():
-                    res[key] = value
-                res['name'] = res['name'].strip()
-                matchObj = re.match(r'\[(.*?)\].*', res['name'], re.M | re.I)
-                res['sig'] = matchObj.group(1).strip()
-                res['duration'] = int(float(res['time']))
-                res['test_desc_file'] = descfile
-                del res['time']
+                    for key, value in testcase.items():
+                        res[key] = value
+                    res['name'] = res['name'].strip()
+                    matchObj = re.match(r'\[(.*?)\].*', res['name'], re.M | re.I)
+                    res['sig'] = matchObj.group(1).strip()
+                    res['duration'] = int(float(res['time']))
+                    res['test_desc_file'] = descfile
+                    del res['time']
 
-                # Write json file
-                with open(filename, 'w') as f:
-                    json_index = {"index": {"_index": index, "_type": "_doc"}}
-                    f.write(json.dumps(json_index)+"\n")
-                    f.write(json.dumps(res))
+                    # Write json file
+                    with open(filename, 'w') as f:
+                        json_index = {"index": {"_index": index, "_type": "_doc"}}
+                        f.write(json.dumps(json_index)+"\n")
+                        f.write(json.dumps(res))
 
     except Exception as e:
         help('ERROR (convtojson.py): something went wrong during XML parsing: {}'.format(e))
@@ -71,7 +75,7 @@ def convert_junit_01_xml_to_json(
     return 0
 
 
-HELPMESSAGE = 'convtojson.py -f|--inputfile <xml inputfile> -i|--index <elastic search index> -t|--targetdir <directory for json files> -d|--descfile <test description file>'
+HELPMESSAGE = 'convtojson.py -f|--inputdir <xml inputdir> -i|--index <elastic search index> -t|--targetdir <directory for json files> -d|--descfile <test description file>'
 
 
 def help(msg=HELPMESSAGE):
@@ -86,7 +90,7 @@ if __name__ == "__main__":
 
     try:
         opts, args = getopt.getopt(sys.argv[1:], "hf:i:t:d:",
-                                   ["inputfile=", "index=", "targetdir=", "descfile="])
+                                   ["inputdir=", "index=", "targetdir=", "descfile="])
     except getopt.GetoptError:
         help()
         sys.exit(2)
@@ -94,8 +98,8 @@ if __name__ == "__main__":
         if opt == '-h':
             help()
             sys.exit()
-        elif opt in ("-f", "--inputfile"):
-            inputfile = arg
+        elif opt in ("-f", "--inputdir"):
+            inputdir = arg
         elif opt in ("-i", "--index"):
             index = arg
         elif opt in ("-t", "--targetdir"):
@@ -103,19 +107,19 @@ if __name__ == "__main__":
         elif opt in ("-d", "--descfile"):
             descfile = arg
 
-    if inputfile == '' or targetdir == '':
+    if inputdir == '' or targetdir == '':
         help()
         sys.exit(1)
 
-    if not os.path.isfile(inputfile):
-        help('ERROR (convtojson.py): file "{}" does not exist'.format(inputfile))
+    if not os.path.isdir(inputdir):
+        help('ERROR (convtojson.py): directory "{}" does not exist'.format(inputdir))
         sys.exit(1)
 
     if not os.path.isdir(targetdir):
         help('ERROR (convtojson.py): directory "{}" does not exist'.format(targetdir))
         sys.exit(1)
 
-    if convert_junit_01_xml_to_json(inputfile, index, targetdir, descfile) is None:
+    if convert_junit_01_xml_to_json(inputdir, index, targetdir, descfile) is None:
         sys.exit(1)
     else:
         sys.exit(0)
