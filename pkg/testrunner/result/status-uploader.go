fe44ed821b34fbe4960fda39491740a8bf86f771
Oleg Loewen <44769983+OlegLoewen@users.noreply.github.com>
2019-11-21 14:05:39 +0100

    Refactor status asset upload (#190)
    Fix: evaluate existing assets and upload assets independently per component. Remove deprecated assets.  unzip asset files to right directory level, to avoid nested files

--- pkg/testrunner/result/status-uploader.go
+++ pkg/testrunner/result/status-uploader.go
@@ -41,97 +41,107 @@ func UploadStatusToGithub(loggerInstance logr.Logger, runs testrunner.RunList, c
 		l.Error(err, "failed to parse components")
 		return err
 	}
-	// need one component to check whether status assets have been uploaded before to corresponding release
-	randomComponent := extendedComponents[0]
 
-	testrunsToUpload, err := identifyTestrunsToUpload(runs, randomComponent, overviewFilepath)
-	if testrunsToUpload == nil || len(testrunsToUpload) == 0 {
-		l.Info("no testrun updates, therefore not assets to upload")
-		return nil
-	}
-	l.Info(fmt.Sprintf("identified %d testruns for github asset upload", len(testrunsToUpload)))
-
-	archiveFilename := fmt.Sprintf("%s%s.zip", prefix, (runs)[0].Metadata.Landscape)
-	extension := filepath.Ext(archiveFilename)
-	archiveFilenameWithoutExtension := archiveFilename[0 : len(archiveFilename)-len(extension)]
-	archiveContentDir := path.Join(dest, archiveFilenameWithoutExtension)
-	archiveFilepath := filepath.Join(dest, archiveFilename)
-	_ = os.Remove(archiveFilepath)
-	_ = os.RemoveAll(archiveContentDir)
-	if err := os.MkdirAll(archiveContentDir, 0777); err != nil {
-		l.Error(err, fmt.Sprintf("failed to create dir: %s", archiveContentDir))
-		return err
-	}
-	remoteArchiveAssetID, err := getAssetIDByName(randomComponent, archiveFilename)
-	if err != nil {
-		l.Error(err, fmt.Sprintf("failed to get asset ID of %s in component %s/%s", archiveFilename, randomComponent.Owner, randomComponent.Name))
-		return err
-	}
-	if remoteArchiveAssetID != 0 {
-		// if status archive file exists, download and unzip it
-		if err := downloadReleaseAssetByName(randomComponent, archiveFilename, archiveFilepath); err != nil {
-			l.Error(err, fmt.Sprintf("failed to download release asset %s in component %s/%s", archiveFilename, randomComponent.Owner, randomComponent.Name))
+	for _, component := range extendedComponents {
+		assetOverview, err := getAssetOverview(component, overviewFilepath)
+		// remove previously failed items, to avoid that after component patch, failed items are kept forever
+		removedTestrunItems := removeFailedItems(&assetOverview)
+		if err := writeOverviewToFile(assetOverview, overviewFilepath); err != nil {
 			return err
 		}
-		l.Info(fmt.Sprintf("unzipping %s into %s", archiveFilename, archiveContentDir))
-		if err := util.Unzip(archiveFilepath, archiveContentDir); err != nil {
-			l.Error(err, fmt.Sprintf("failed to unzip %s", archiveFilepath))
+		testrunsToUpload, err := identifyTestrunsToUpload(runs, assetOverview)
+		if testrunsToUpload == nil || len(testrunsToUpload) == 0 {
+			l.Info("no testrun updates, therefore not assets to upload")
+			return nil
+		}
+		l.Info(fmt.Sprintf("identified %d testruns for github asset upload", len(testrunsToUpload)))
+
+		archiveFilename := fmt.Sprintf("%s%s.zip", prefix, (runs)[0].Metadata.Landscape)
+		extension := filepath.Ext(archiveFilename)
+		archiveFilenameWithoutExtension := archiveFilename[0 : len(archiveFilename)-len(extension)]
+		archiveContentDir := path.Join(dest, archiveFilenameWithoutExtension)
+		archiveFilepath := filepath.Join(dest, archiveFilename)
+		_ = os.Remove(archiveFilepath)
+		_ = os.RemoveAll(archiveContentDir)
+		if err := os.MkdirAll(archiveContentDir, 0777); err != nil {
+			l.Error(err, fmt.Sprintf("failed to create dir: %s", archiveContentDir))
 			return err
 		}
-	}
-	if err := storeRunsStatusAsFiles(testrunsToUpload, archiveContentDir); err != nil {
-		l.Error(err, "Failed to store testrun status as files")
-		return err
-	}
-	if err := util.Zipit(archiveContentDir, archiveFilepath); err != nil {
-		l.Error(err, fmt.Sprintf("Failed to zip %s", archiveContentDir))
-		return err
-	}
-	if err := createOrUpdateOverview(overviewFilepath, testrunsToUpload); err != nil {
-		l.Error(err, fmt.Sprintf("Failed to create/update %s", overviewFilepath))
-		return err
-	}
+		remoteArchiveAssetID, err := getAssetIDByName(component, archiveFilename)
+		if err != nil {
+			l.Error(err, fmt.Sprintf("failed to get asset ID of %s in component %s/%s", archiveFilename, component.Owner, component.Name))
+			return err
+		}
+		if remoteArchiveAssetID != 0 {
+			// if status archive file exists, download and unzip it
+			if err := downloadReleaseAssetByName(component, archiveFilename, archiveFilepath); err != nil {
+				l.Error(err, fmt.Sprintf("failed to download release asset %s in component %s/%s", archiveFilename, component.Owner, component.Name))
+				return err
+			}
+			l.Info(fmt.Sprintf("unzipping %s into %s", archiveFilename, archiveContentDir))
+			if err := util.Unzip(archiveFilepath, filepath.Dir(archiveContentDir)); err != nil {
+				l.Error(err, fmt.Sprintf("failed to unzip %s", archiveFilepath))
+				return err
+			}
+			for _, fileToRemove := range removedTestrunItems {
+				filepathToRemove := filepath.Join(archiveContentDir, fileToRemove)
+				if err := os.Remove(filepathToRemove); err != nil {
+					l.Info(fmt.Sprintf("failed to remove failed testrun file %s", filepathToRemove), err)
+				}
+			}
+		}
+		if err := storeRunsStatusAsFiles(testrunsToUpload, archiveContentDir); err != nil {
+			l.Error(err, "Failed to store testrun status as files")
+			return err
+		}
+		if err := util.Zipit(archiveContentDir, archiveFilepath); err != nil {
+			l.Error(err, fmt.Sprintf("Failed to zip %s", archiveContentDir))
+			return err
+		}
+		if err := createOrUpdateOverview(overviewFilepath, testrunsToUpload); err != nil {
+			l.Error(err, fmt.Sprintf("Failed to create/update %s", overviewFilepath))
+			return err
+		}
 
-	var filesToUpload []string
-	filesToUpload = append(filesToUpload, overviewFilepath)
-	filesToUpload = append(filesToUpload, archiveFilepath)
+		var filesToUpload []string
+		filesToUpload = append(filesToUpload, overviewFilepath)
+		filesToUpload = append(filesToUpload, archiveFilepath)
 
-	if err := uploadFiles(extendedComponents, filesToUpload); err != nil {
-		return err
+		if err := uploadFiles(component, filesToUpload); err != nil {
+			return err
+		}
 	}
 
 	return nil
 }
 
 // uploads files to github component releases as assets
-func uploadFiles(components []ComponentExtended, files []string) error {
-	for _, c := range components {
-		for _, filepathToUpload := range files {
-			l.Info(fmt.Sprintf("uploading asset %s to %s/%s", filepath.Base(filepathToUpload), c.Owner, c.Name))
-			file, err := os.Open(filepathToUpload)
-			if err != nil {
-				l.Error(err, fmt.Sprintf("Can't open file %s", filepathToUpload))
-				return err
-			}
-			defer file.Close()
-			filename := filepath.Base(filepathToUpload)
-			uploadOptions := github.UploadOptions{Name: filename}
+func uploadFiles(c ComponentExtended, files []string) error {
+	for _, filepathToUpload := range files {
+		l.Info(fmt.Sprintf("uploading asset %s to %s/%s", filepath.Base(filepathToUpload), c.Owner, c.Name))
+		file, err := os.Open(filepathToUpload)
+		if err != nil {
+			l.Error(err, fmt.Sprintf("Can't open file %s", filepathToUpload))
+			return err
+		}
+		defer file.Close()
+		filename := filepath.Base(filepathToUpload)
+		uploadOptions := github.UploadOptions{Name: filename}
 
-			// delete previous remote asset, since a new one will be uploaded
-			if err := deleteAssetIfExists(c, filename); err != nil {
-				l.Error(err, fmt.Sprintf("Can't open file %s", filepathToUpload))
-				return err
-			}
+		// delete previous remote asset, since a new one will be uploaded
+		if err := deleteAssetIfExists(c, filename); err != nil {
+			l.Error(err, fmt.Sprintf("Can't open file %s", filepathToUpload))
+			return err
+		}
 
-			_, response, err := c.GithubClient.Repositories.UploadReleaseAsset(context.Background(), c.Owner, c.Name, c.GithubReleaseID, &uploadOptions, file)
-			if err != nil {
-				l.Error(err, fmt.Sprintf("Was not able to upload %s release asset %s/%s", file.Name(), c.Owner, c.Name))
-				return err
-			} else if response.StatusCode != 201 {
-				err := errors.New(fmt.Sprintf("Asset upload failed with status code %d", response.StatusCode))
-				l.Error(err, "")
-				return err
-			}
+		_, response, err := c.GithubClient.Repositories.UploadReleaseAsset(context.Background(), c.Owner, c.Name, c.GithubReleaseID, &uploadOptions, file)
+		if err != nil {
+			l.Error(err, fmt.Sprintf("Was not able to upload %s release asset %s/%s", file.Name(), c.Owner, c.Name))
+			return err
+		} else if response.StatusCode != 201 {
+			err := errors.New(fmt.Sprintf("Asset upload failed with status code %d", response.StatusCode))
+			l.Error(err, "")
+			return err
 		}
 	}
 	return nil
@@ -171,6 +181,13 @@ func createOrUpdateOverview(overviewFilepath string, testrunsToUpload testrunner
 		}
 		assetOverview.AssetOverviewItems = append(assetOverview.AssetOverviewItems, AssetOverviewItem{Name: assetItemName, Successful: isAssetItemSuccessful})
 	}
+	if err := writeOverviewToFile(assetOverview, overviewFilepath); err != nil {
+		return err
+	}
+	return nil
+}
+
+func writeOverviewToFile(assetOverview AssetOverview, overviewFilepath string) error {
 	overviewJSONBytes, err := json.MarshalIndent(assetOverview, "", "   ")
 	if err != nil {
 		l.Error(err, fmt.Sprintf("failed to marshal %s", overviewFilepath))
@@ -206,25 +223,8 @@ func generateTestrunAssetName(testrun testrunner.Run) string {
 }
 
 // compares overview file items with given testrun list to identify whether any testrun is missing or needs to be updated
-func identifyTestrunsToUpload(runs testrunner.RunList, component ComponentExtended, overviewFilepath string) (testrunner.RunList, error) {
-	_ = os.Remove(overviewFilepath) // try to remove previously downloaded file
-	remoteAssetID, err := getAssetIDByName(component, filepath.Base(overviewFilepath))
-	if err != nil {
-		return nil, err
-	}
-	if remoteAssetID == 0 {
-		// if no status overview file exists upload results of all testruns
-		return runs, nil
-	}
-	if err := downloadReleaseAssetByName(component, filepath.Base(overviewFilepath), overviewFilepath); err != nil {
-		return nil, err
-	}
+func identifyTestrunsToUpload(runs testrunner.RunList, assetOverview AssetOverview) (testrunner.RunList, error) {
 	var testrunsToUpload testrunner.RunList
-	assetOverview, err := unmarshalOverview(overviewFilepath)
-	if err != nil {
-		return nil, err
-	}
-
 	for _, run := range runs {
 		testrunAssetName := generateTestrunAssetName(*run)
 		testrunSuccessful := run.Testrun.Status.Phase == tmv1beta1.PhaseStatusSuccess
@@ -233,6 +233,40 @@ func identifyTestrunsToUpload(runs testrunner.RunList, component ComponentExtend
 		}
 	}
 	return testrunsToUpload, nil
+}
+
+func getAssetOverview(component ComponentExtended, overviewFilepath string) (AssetOverview, error) {
+	_ = os.Remove(overviewFilepath) // try to remove previously downloaded file
+	emptyOverview := AssetOverview{}
+	remoteAssetID, err := getAssetIDByName(component, filepath.Base(overviewFilepath))
+	if err != nil {
+		return emptyOverview, err
+	}
+	if remoteAssetID == 0 {
+		// if no status overview file exists upload results of all testruns
+		return emptyOverview, nil
+	}
+	if err := downloadReleaseAssetByName(component, filepath.Base(overviewFilepath), overviewFilepath); err != nil {
+		return emptyOverview, err
+	}
+	assetOverview, err := unmarshalOverview(overviewFilepath)
+	if err != nil {
+		return emptyOverview, err
+	}
+	return assetOverview, nil
+}
+
+func removeFailedItems(assetOverview *AssetOverview) (failedOverviewItems []string) {
+	var successfulOverviewItems []AssetOverviewItem
+	for _, item := range assetOverview.AssetOverviewItems  {
+		if item.Successful {
+			successfulOverviewItems = append(successfulOverviewItems, item)
+		} else {
+			failedOverviewItems = append(failedOverviewItems, item.Name)
+		}
+	}
+	assetOverview.AssetOverviewItems = successfulOverviewItems
+	return failedOverviewItems
 }
 
 func unmarshalOverview(overviewFilepath string) (AssetOverview, error) {
