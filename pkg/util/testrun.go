56ecad903bd8f167c6fc4d05785375c13389c250
Tim Schrodi <tim.schrodi@sap.com>
2020-02-14 08:40:16 +0100

    order steps in testrunner table output

--- pkg/util/testrun.go
+++ pkg/util/testrun.go
@@ -18,38 +18,39 @@ import (
 	"context"
 	"fmt"
 	argov1alpha1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
-	"github.com/gardener/test-infra/pkg/apis/testmachinery/v1beta1"
+	tmv1beta1 "github.com/gardener/test-infra/pkg/apis/testmachinery/v1beta1"
 	"github.com/gardener/test-infra/pkg/common"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sort"
 )
 
 // TestrunStatusPhase determines the real testrun phase of a testrun by ignoring exit handler failures and system component failures if all other tests passed.
-func TestrunStatusPhase(tr *v1beta1.Testrun) argov1alpha1.NodePhase {
-	if tr.Status.Phase == v1beta1.PhaseStatusSuccess {
-		return v1beta1.PhaseStatusSuccess
+func TestrunStatusPhase(tr *tmv1beta1.Testrun) argov1alpha1.NodePhase {
+	if tr.Status.Phase == tmv1beta1.PhaseStatusSuccess {
+		return tmv1beta1.PhaseStatusSuccess
 	}
 
 	stepsRun := false
 	for _, step := range tr.Status.Steps {
-		if !stepsRun && (step.Phase != v1beta1.PhaseStatusInit && step.Phase != v1beta1.PhaseStatusSkipped) {
+		if !stepsRun && (step.Phase != tmv1beta1.PhaseStatusInit && step.Phase != tmv1beta1.PhaseStatusSkipped) {
 			stepsRun = true
 		}
-		if step.Phase == v1beta1.PhaseStatusInit || step.Phase == v1beta1.PhaseStatusSkipped {
+		if step.Phase == tmv1beta1.PhaseStatusInit || step.Phase == tmv1beta1.PhaseStatusSkipped {
 			continue
 		}
-		if step.Phase != v1beta1.PhaseStatusSuccess && step.Annotations[common.AnnotationSystemStep] != "true" {
+		if step.Phase != tmv1beta1.PhaseStatusSuccess && step.Annotations[common.AnnotationSystemStep] != "true" {
 			return step.Phase
 		}
 	}
 
 	if stepsRun {
-		return v1beta1.PhaseStatusSuccess
+		return tmv1beta1.PhaseStatusSuccess
 	}
 
 	return tr.Status.Phase
 }
 
-func IsSystemStep(step *v1beta1.StepStatus) bool {
+func IsSystemStep(step *tmv1beta1.StepStatus) bool {
 	if len(step.Annotations) == 0 {
 		return false
 	}
@@ -60,7 +61,7 @@ func IsSystemStep(step *v1beta1.StepStatus) bool {
 }
 
 // Resume testruns resumes a testrun by adding the appropriate annotation to it
-func ResumeTestrun(ctx context.Context, k8sClient client.Client, tr *v1beta1.Testrun) error {
+func ResumeTestrun(ctx context.Context, k8sClient client.Client, tr *tmv1beta1.Testrun) error {
 	obj, err := client.ObjectKeyFromObject(tr)
 	if err != nil {
 		return err
@@ -80,17 +81,33 @@ func ResumeTestrun(ctx context.Context, k8sClient client.Client, tr *v1beta1.Tes
 }
 
 // TestrunProgress returns the progress of a testrun
-func TestrunProgress(tr *v1beta1.Testrun) string {
+func TestrunProgress(tr *tmv1beta1.Testrun) string {
 	allSteps := 0
 	completedSteps := 0
 	for _, step := range tr.Status.Steps {
 		if step.Annotations[common.AnnotationSystemStep] != "true" {
 			allSteps++
-			if step.Phase != v1beta1.PhaseStatusSkipped && Completed(step.Phase) {
+			if step.Phase != tmv1beta1.PhaseStatusSkipped && Completed(step.Phase) {
 				completedSteps++
 			}
 		}
 	}
 
 	return fmt.Sprintf("%d/%d", completedSteps, allSteps)
+}
+
+// OrderStepsStatus orders a list of step status of a testrun
+func OrderStepsStatus(steps []*tmv1beta1.StepStatus) {
+	sort.Sort(stepStatusList(steps))
+}
+
+type stepStatusList []*tmv1beta1.StepStatus
+
+func (l stepStatusList) Len() int      { return len(l) }
+func (l stepStatusList) Swap(i, j int) { l[i], l[j] = l[j], l[i] }
+func (l stepStatusList) Less(i, j int) bool {
+	if l[i].StartTime == nil || l[j].StartTime == nil {
+		return true
+	}
+	return l[j].StartTime.Before(l[i].StartTime)
 }
