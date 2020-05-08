0440afb06242ee6006ffc19a14df4f7c0b68d838
Tim Schrodi <tim.schrodi@sap.com>
2020-05-08 11:29:20 +0200

    Add publish of the testmachinery helm chart (#278)

--- hack/set-minio-env.sh
+++
@@ -1,3 +0,0 @@
-export S3_ENDPOINT=$(minikube service minio-service --url | xargs basename)
-export S3_ACCESS_KEY=$(cat charts/bootstrap_tm_prerequisites/values.yaml | yq r - 'objectStorage.secret.accessKey')
-export S3_SECRET_KEY=$(cat charts/bootstrap_tm_prerequisites/values.yaml | yq r - 'objectStorage.secret.secretKey')
