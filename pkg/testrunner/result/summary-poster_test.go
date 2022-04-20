aaa5f8d8f09e9d8831ff23ad91e84a269e5bfae6
Dieter Guendisch <dieter.guendisch@sap.com>
2022-04-20 11:05:12 +0200

    implemented review suggestions

--- pkg/testrunner/result/summary-poster_test.go
+++ pkg/testrunner/result/summary-poster_test.go
@@ -1,17 +1,32 @@
+// Copyright 2022 Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package result
 
 import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
 	tmv1beta1 "github.com/gardener/test-infra/pkg/apis/testmachinery/v1beta1"
 	"github.com/gardener/test-infra/pkg/testmachinery/metadata"
 	"github.com/gardener/test-infra/pkg/testrunner"
 	"github.com/gardener/test-infra/pkg/util"
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("summary-poster", func() {
 
-	It("should render summary with 1 and 2 results for the same line", func() {
+	It("should render summary with single result", func() {
 		run := &testrunner.Run{
 			Info: nil,
 			Testrun: &tmv1beta1.Testrun{
@@ -33,7 +48,24 @@ var _ = Describe("summary-poster", func() {
 		Expect(err).ToNot(HaveOccurred())
 		Expect(slack).To(ContainSubstring(string(util.StatusSymbolSuccess)))
 		Expect(slack).ToNot(ContainSubstring(string(util.StatusSymbolNA)))
+	})
 
+	It("should render summary with two results for the same line", func() {
+		var runs testrunner.RunList
+		run := &testrunner.Run{
+			Info: nil,
+			Testrun: &tmv1beta1.Testrun{
+				Status: tmv1beta1.TestrunStatus{
+					Phase: tmv1beta1.RunPhaseSuccess,
+				},
+			},
+			Metadata: &metadata.Metadata{
+				CloudProvider: "foo",
+			},
+			Error:      nil,
+			Rerenderer: nil,
+		}
+		runs = append(runs, run)
 		run = &testrunner.Run{
 			Info: nil,
 			Testrun: &tmv1beta1.Testrun{
@@ -48,9 +80,9 @@ var _ = Describe("summary-poster", func() {
 			Rerenderer: nil,
 		}
 		runs = append(runs, run)
-		items = parseTestrunsToTableItems(runs)
+		items := parseTestrunsToTableItems(runs)
 		Expect(items).To(HaveLen(2))
-		slack, err = util.RenderTableForSlack(nil, items)
+		slack, err := util.RenderTableForSlack(nil, items)
 		Expect(err).ToNot(HaveOccurred())
 		Expect(slack).To(ContainSubstring(string(util.StatusSymbolSuccess) + util.SymbolOffset + string(util.StatusSymbolFailure)))
 		Expect(slack).ToNot(ContainSubstring(string(util.StatusSymbolNA)))
