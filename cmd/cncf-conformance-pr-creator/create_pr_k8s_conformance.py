a12e0e684fac4ba91b101d0c900ed26ba6022f34
Dieter Guendisch <dieter.guendisch@sap.com>
2022-12-09 17:45:12 +0100

    adapt test result parsing to K8s v1.25

--- cmd/cncf-conformance-pr-creator/create_pr_k8s_conformance.py
+++ cmd/cncf-conformance-pr-creator/create_pr_k8s_conformance.py
@@ -264,14 +264,17 @@ def isConformanceTestSuccessful(e2e_log_file, k8s_version):
         print("Error: file " + e2e_log_file + " does not exist.")
         sys.exit(1)
     isConformanceTestSuccessful = False
-    log_status_line_number = -5
-    if k8s_version in ['v1.10', 'v1.11', 'v1.12', 'v1.13', 'v1.14']:
-        log_status_line_number = -4
-    with open(e2e_log_file) as fp:
-        e2e_log_status_line = (list(fp)[log_status_line_number])
-    pattern = re.compile("^(FAIL|SUCCESS).*Passed.*Failed.*")
-    if not pattern.match(e2e_log_status_line):
-        print("Error: " + str(log_status_line_number) + " line of e2e.log is not of format ^(FAIL|SUCCESS).*Passed.*Failed.*$. Actual line content: '" + e2e_log_status_line + "'")
+    pattern = "^(FAIL|SUCCESS).*Passed.*Failed.*"
+    e2e_log_status_line = ""
+
+    with open(e2e_log_file) as f:
+        for line in f:
+            match = re.search(pattern, line)
+            if match:
+                e2e_log_status_line = match[0]
+                break
+    if not e2e_log_status_line:
+        print("Error: line of e2e log is not of format " + pattern + ". Actual line content: '" + e2e_log_status_line + "'")
         return False
     if conformance_tests_passed_string in e2e_log_status_line:
         # this is certification requirement
