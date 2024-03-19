edb4e4510f30cf60d99d0d1ef1b512e04f7c67e4
hendrikKahl <39563366+hendrikKahl@users.noreply.github.com>
2024-03-19 09:17:57 +0100

    Testrunner adds a providerconfig to any AWS worker (#509)
    Shoots on AWS need to be created with IMDSv2 by default. This can be
    enabled through worker's providerConfig.
    Testrunner will add a suitable proivderConfig when parsing the worker
    configuration from the shoot flavor to extend any AWS worker.

--- pkg/shootflavors/worker.go
+++ pkg/shootflavors/worker.go
@@ -1,7 +1,13 @@
 package shootflavors
 
 import (
+	"encoding/json"
+
+	"github.com/gardener/gardener-extension-provider-aws/pkg/apis/aws/v1alpha1"
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/utils/ptr"
 
 	"github.com/gardener/test-infra/pkg/common"
 	"github.com/gardener/test-infra/pkg/util"
@@ -17,6 +23,26 @@ func SetupWorker(cloudprofile gardencorev1beta1.CloudProfile, workers []gardenco
 				return nil, err
 			}
 			worker.Machine.Image.Version = &version.Version
+		}
+
+		if cloudprofile.Spec.Type == "aws" {
+			providerConfig := v1alpha1.WorkerConfig{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "WorkerConfig",
+					APIVersion: "aws.provider.extensions.gardener.cloud/v1alpha1",
+				},
+				InstanceMetadataOptions: &v1alpha1.InstanceMetadataOptions{
+					HTTPTokens:              ptr.To(v1alpha1.HTTPTokensRequired),
+					HTTPPutResponseHopLimit: ptr.To(int64(2)),
+				},
+			}
+			js, err := json.Marshal(providerConfig)
+			if err != nil {
+				return nil, err
+			}
+			worker.ProviderConfig = &runtime.RawExtension{
+				Raw: js,
+			}
 		}
 		res[i] = *worker
 	}
