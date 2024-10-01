a940191c0241f1635a9090df3ba1f787f782c9b4
hendrikKahl <hendrik.kahl@sap.com>
2024-10-01 14:42:37 +0200

    add retry logic and pin hydrophone version

--- conformance-tests/hydrophone/hydrophone.go
+++ conformance-tests/hydrophone/hydrophone.go
@@ -5,14 +5,21 @@
 package hydrophone
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"os/exec"
+	controllerruntime "sigs.k8s.io/controller-runtime"
 	"strconv"
+	"time"
 
 	"github.com/go-logr/logr"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/gardener/test-infra/conformance-tests/config"
+	"github.com/gardener/test-infra/pkg/util/kubernetes"
 )
 
 // Setup installs a given version of Hydrophone using the "go install" command
@@ -26,31 +33,52 @@ func Setup(log logr.Logger) error {
 func Run(log logr.Logger) error {
 	log.Info("Starting Conformance tests with Hydrophone")
 
-	hydrophoneArgs := []string{
+	hydrophoneBaseArgs := []string{
 		"--kubeconfig", config.ShootKubeconfigPath,
 		"-o", config.ExportPath,
-		"--conformance",
 		"-v", strconv.Itoa(config.ConformanceLogLevel),
+	}
+
+	hydrophoneRunArgs := []string{
+		"--conformance",
 		"--conformance-image", fmt.Sprintf("registry.k8s.io/conformance:v%s", config.K8sRelease),
 	}
 
 	if config.SkipIndividualTestCases != "" {
-		hydrophoneArgs = append(hydrophoneArgs, "--skip", config.SkipIndividualTestCases)
+		hydrophoneRunArgs = append(hydrophoneRunArgs, "--skip", config.SkipIndividualTestCases)
 	}
 
 	if config.DryRun {
-		hydrophoneArgs = append(hydrophoneArgs, "--dry-run")
+		hydrophoneRunArgs = append(hydrophoneRunArgs, "--dry-run")
 	}
 
 	// ginkgo cannot do a dry-run with multiple nodes
 	if config.GinkgoParallel && !config.DryRun {
-		hydrophoneArgs = append(hydrophoneArgs, "-p", "8")
+		hydrophoneRunArgs = append(hydrophoneRunArgs, "-p", "8")
 	}
 	if config.FlakeAttempts != 1 {
-		hydrophoneArgs = append(hydrophoneArgs, "--extra-ginkgo-args", fmt.Sprintf("--flake-attempts=%d", config.FlakeAttempts))
+		hydrophoneRunArgs = append(hydrophoneRunArgs, "--extra-ginkgo-args", fmt.Sprintf("--flake-attempts=%d", config.FlakeAttempts))
 	}
 
-	return runCommand(log, "hydrophone", hydrophoneArgs...)
+	controllerruntime.SetLogger(log)
+	shootClient, err := kubernetes.NewClientFromFile(config.ShootKubeconfigPath, client.Options{})
+	if err != nil {
+		return err
+	}
+
+	err = runCommand(log, "hydrophone", append(hydrophoneBaseArgs, hydrophoneRunArgs...)...)
+	if err != nil && isRetryable(log.WithName("IsRetryable"), shootClient) {
+		for {
+			log.Info("Hydrophone exited with a retryable error")
+			time.Sleep(10 * time.Second)
+			err = runCommand(log, "hydrophone", append(hydrophoneBaseArgs, "--continue")...)
+			if err == nil || !isRetryable(log.WithName("IsRetryable"), shootClient) {
+				return err
+			}
+		}
+	}
+	return err
+
 }
 
 // runCommand constructs a command, logs it with all arguments and executes it.
@@ -60,4 +88,46 @@ func runCommand(log logr.Logger, command string, args ...string) error {
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
 	return cmd.Run()
+}
+
+// isRetryable returns true if the conformance namespace is still active and an e2e pod exists.
+func isRetryable(log logr.Logger, cl client.Client) bool {
+	retries := 6
+	backOff := 1 * time.Second
+	for {
+		backOff = 2 * backOff
+		conformanceNamespace := &v1.Namespace{}
+		if err := cl.Get(context.Background(), client.ObjectKey{Name: "conformance"}, conformanceNamespace); err != nil {
+			if errors.IsNotFound(err) {
+				log.Info("Conformance namespace does not exist anymore")
+				return false
+			}
+			log.Error(err, "A request to the kube-apiserver of the shoot failed")
+			retries--
+			if retries == 0 {
+				return false
+			}
+			time.Sleep(backOff)
+			continue
+		}
+		if conformanceNamespace.Status.Phase != v1.NamespaceActive {
+			return false
+		}
+		conformancePod := &v1.Pod{}
+		if err := cl.Get(context.Background(), client.ObjectKey{Namespace: "conformance", Name: "e2e-conformance-test"}, conformancePod); err != nil {
+			if errors.IsNotFound(err) {
+				log.Info("e2e-conformance-test Pod does not exist anymore")
+				return false
+			}
+			log.Error(err, "A request to the kube-apiserver of the shoot failed")
+			retries--
+			if retries == 0 {
+				return false
+			}
+			time.Sleep(backOff)
+			continue
+		}
+		break
+	}
+	return true
 }
