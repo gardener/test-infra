4334502a014a49f7dc59ca7585e154a12fd6ee80
hendrikKahl <hendrik.kahl@sap.com>
2025-07-23 15:45:58 +0200

    use source info from component descriptor

--- pkg/testrunner/componentdescriptor/types.go
+++ pkg/testrunner/componentdescriptor/types.go
@@ -4,16 +4,41 @@
 package componentdescriptor
 
 import (
-	common "ocm.software/ocm/api/utils/misc"
+	"ocm.software/ocm/api/ocm"
+	"ocm.software/ocm/api/ocm/compdesc"
+	"ocm.software/ocm/api/ocm/extensions/accessmethods/github"
 )
 
 // Component describes a component consisting of the git repository url and a version.
 type Component struct {
-	Name    string `json:"name"`
-	Version string `json:"version"`
+	Name           string `json:"name"`
+	Version        string `json:"version"`
+	SourceRepoURL  string `json:"sourceRepoUrl"`
+	SourceRevision string `json:"sourceRevision"`
 }
 
-func NewFromVersionedElement(e common.VersionedElement) *Component {
+func NewFromVersionedElement(octx ocm.Context, e compdesc.ComponentDescriptor) *Component {
+	sources := e.GetSources()
+	for _, source := range sources {
+		var label compdesc.Label
+		exists, _ := source.Labels.GetValue("cloud.gardener/cicd/source", label)
+		if exists {
+			access := source.GetAccess()
+			accessSpec, err := octx.AccessSpecForSpec(access)
+			if err == nil {
+				ghacces, ok := accessSpec.(*github.AccessSpec)
+				if ok {
+					return &Component{
+						Name:           e.GetName(),
+						Version:        e.GetVersion(),
+						SourceRepoURL:  ghacces.RepoURL,
+						SourceRevision: source.Version,
+					}
+				}
+			}
+		}
+	}
+
 	return &Component{
 		Name:    e.GetName(),
 		Version: e.GetVersion(),
